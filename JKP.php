7588afbff0bcc61acbe5d866cafbadd9f34b57a4
unknown <imthedank69@gmail.com>
2024-09-06 00:05:07 +0700

    New Processing function

--- JKP.php
+++ JKP.php
@@ -67,18 +67,23 @@ class JKP {
             }
             if($OurPick == 3){
                 if($OppPick == $OurPick){
+                    echo "<style> .back-button { margin-top: 0px; }</style>";
                     echo "<center><img src='./assets/img/SvS.png'><br>";
                     echo "<h1>Scissor vs Scissor</h1><br>";
                     echo "<p class='battle-result-tie'><b>tie</b></p>";
                 }else{
                     if($OppPick == 1){
                         $_SESSION['Wcounter'] -= 1;
+
+                        echo "<style> .back-button { margin-top: 0px; }</style>";
                         echo "<center><img src='./assets/img/SvR.png'><br>";
                         echo "<h1>Scissor vs Rock</h1><br>";
                         echo "<p class='battle-result-lose'><b>Lose</b></p>";
                     }else{
                         if($OppPick == 2){
                             $_SESSION['Wcounter'] += 1;
+
+                            echo "<style> .back-button { margin-top: 0px; }</style>";
                             echo "<center><img src='./assets/img/SvP.png'><br>";
                             echo "<h1>Scissor vs Paper</h1><br>";
                             echo "<p class='battle-result-win'><b>Win</b></p>";
@@ -87,5 +92,77 @@ class JKP {
                 }
             }
         
+    }
+
+    // 1 = rock
+    // 2 = paper
+    // 3 = scisscor
+
+    public function jankenpon_v2() {
+
+        $OppPick = rand(1,3);
+
+        if(isset($_POST['rock'])){
+            if($OppPick == 1){
+                echo "<center><img src='./assets/img/Rvr.png'><br>";
+                echo "<h1>Rock vs Rock</h1><br>";
+                echo "<b class='battle-result-tie'>tie</b>";
+            }else{
+                if($OppPick == 2){
+                    echo "<center><img src='./assets/img/RvP.png'><br>";
+                    echo "<h1>Rock vs Paper</h1><br>";
+                    echo "<b class='battle-result-lose'>Lose</b>";
+                }else{
+                    if($OppPick == 3){
+                        echo "<center><img src='./assets/img/RvS.png'><br>";
+                        echo "<h1>Rock vs Scissor</h1><br>";
+                        echo "<b class='battle-result-win'>Win</b>";
+                    }
+                }
+            }
+        }
+
+        if(isset($_POST['paper'])){
+            if($OppPick == 1){
+                echo "<center><img src='./assets/img/PvR.png'><br>";
+                echo "<h1>Paper vs Scissor</h1><br>";
+                echo "<b class='battle-result-win'>Win</b>";
+            }else{
+                if($OppPick == 2){
+                    echo "<center><img src='./assets/img/PvP.png'><br>";
+                    echo "<h1>Paper vs Paper</h1><br>";
+                    echo "<b class='battle-result-tie'>tie</b>";
+                }else{
+                    if($OppPick == 3){
+                        echo "<center><img src='./assets/img/PvS.png'><br>";
+                        echo "<h1>Paper vs Rock</h1><br>";
+                        echo "<b class='battle-result-lose'>Lose</b>";
+                    }
+                }
+            }
+        }
+
+        if(isset($_POST['scissor'])){
+            if($OppPick == 1){
+                echo "<style> .back-button, .res { margin-top: 0px; margin-bottom: 0px;}</style>";
+                echo "<center><img src='./assets/img/SvR.png'><br>";
+                echo "<h1 class='res'>Scissor vs Rock</h1><br>";
+                echo "<p class='battle-result-lose'><b>Lose</b></p>";;
+            }else{
+                if($OppPick == 2){
+                    echo "<style> .back-button, .res { margin-top: 0px; } </style>";
+                    echo "<center><img src='./assets/img/SvP.png'><br>";
+                    echo "<h1 class='res'>Scissor vs Rock</h1><br>";
+                    echo "<p class='battle-result-win'><b>Win</b></p>";
+                }else{
+                    if($OppPick == 3){
+                        echo "<style> .back-button, .res { margin-top: 0px; margin-bottom: 0px;}</style>";
+                        echo "<center><img src='./assets/img/SvS.png'><br>";
+                        echo "<h1 class='res'>Scissor vs Scissor</h1><br>";
+                        echo "<p class='battle-result-tie'><b>tie</b></p>";
+                    }
+                }
+            }
+        }
     }
 }
