42ed72a6c4fabdb7f62a047508baa8680d8c85df
unknown <imthedank69@gmail.com>
2024-09-06 00:03:19 +0700

    Add new UI

--- index.php
+++ index.php
@@ -1,54 +1,10 @@
 <?php require("./part/header.php"); require_once("./part/footer.php");?>
-<style>
-    select{
 
-        width: 30%;
-        height: 50px;
-        font-size: 20px;
-        text-align: center;
-        margin-bottom: 10px;
-    }
-
-    /* CSS */
-    .button-31 {
-    background-color: #222;
-    border-radius: 4px;
-    border-style: none;
-    box-sizing: border-box;
-    color: #fff;
-    cursor: pointer;
-    display: inline-block;
-    font-family: "Farfetch Basis","Helvetica Neue",Arial,sans-serif;
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1.5;
-    margin: 0;
-    max-width: none;
-    min-height: 44px;
-    min-width: 10px;
-    outline: none;
-    overflow: hidden;
-    padding: 9px 20px 8px;
-    position: relative;
-    text-align: center;
-    text-transform: none;
-    user-select: none;
-    -webkit-user-select: none;
-    touch-action: manipulation;
-    width: 15%;
-    }
-
-    .button-31:hover,
-    .button-31:focus {
-    opacity: .75;
-    }
-</style>
-
-<form method="post" action="jankenpon.php" class="jkp">
-    <select name="jankenpon">
-        <option value='1'>Rock</option>
-        <option value='2'>Paper</option>
-        <option value='3'>Scissor</option>
-    </select><br>
-    <input type="submit" name="submit" class="button-31">
-</form>
+<center>
+    <form method="post" action="jankenpon.php" class="jkp">
+        <input type="submit" name="rock" class="button-rock unv-btn" value="Rock">
+        <input type="submit" name="paper" class="button-paper unv-btn" value="Paper">
+        <input type="submit" name="scissor" class="button-scissor unv-btn" value="Scissor">
+    </form>
+    <H1>choose one!</H1>
+</center>
