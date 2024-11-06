0ae074eb0c4f3b9c17a8f4921c5a8e5ea8272bc2
unknown <imthedank69@gmail.com>
2024-11-06 12:03:36 +0700

    (UNFINISHED)

---
+++ assets/script/loading.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+  }
+  
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
